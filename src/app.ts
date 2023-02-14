96f80a8c0c2ecdb1e94c371c45132e6f24c35020
Ludson96 <ludson_ps27@hotmail.com>
2023-02-13 21:17:34 -0300

    feat: add route in app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import express from 'express';
+import ErrorHandler from './Middlewares/ErrorHandler';
+import CarRouter from './Routes/CarRoute';
 
 const app = express();
+app.use(express.json());
+
+app.use('/cars', CarRouter);
+
+app.use(ErrorHandler.handle);
 
 export default app;

5e72eecc2b1e5e05efb9b316c283a9e93905e86b
Ludson96 <ludson_ps27@hotmail.com>
2023-02-14 18:32:56 -0300

    feat: add and refactory Domain

--- src/Domains/Car.ts
+++ src/Domains/Car.ts
@@ -1,16 +1,12 @@
 import ICar from '../Interfaces/ICar';
+import Vehicle from './Vehicle';
 
-export default class Car {
-  protected id: string | undefined;
-  protected model: string;
-  protected year: number;
-  protected color: string;
-  protected status: boolean;
-  protected buyValue: number;
+export default class Car extends Vehicle {
   private doorsQty: number;
   private seatsQty: number;
 
   constructor(car: ICar) {
+    super(car);
     this.id = car.id;
     this.model = car.model;
     this.year = car.year;
@@ -19,54 +15,6 @@ export default class Car {
     this.buyValue = car.buyValue;
     this.doorsQty = car.doorsQty;
     this.seatsQty = car.seatsQty;
-  }
-
-  public setId(id: string) {
-    this.id = id;
-  }
-
-  public getId() {
-    return this.id;
-  }
-
-  public setModel(car: ICar) {
-    this.model = car.model;
-  }
-
-  public getModel() {
-    return this.model;
-  }
-
-  public setYear(car: ICar) {
-    this.year = car.year;
-  }
-
-  public getYear() {
-    return this.year;
-  }
-
-  public setColor(car: ICar) {
-    this.color = car.color;
-  }
-
-  public getColor() {
-    return this.color;
-  }
-
-  public setStatus(status: boolean) {
-    this.status = status;
-  }
-
-  public getStatus() {
-    return this.status;
-  }
-
-  public setBuyValue(car: ICar) {
-    this.buyValue = car.buyValue;
-  }
-
-  public getBuyValue() {
-    return this.buyValue;
   }
 
   public setDoorsQty(car: ICar) {
--- src/Domains/Motorcycle.ts
+++ src/Domains/Motorcycle.ts
@@ -1,16 +1,12 @@
+import Vehicle from './Vehicle';
 import IMotorcycles from '../Interfaces/IMotorcycle';
 
-export default class Motorcycles {
-  protected id: string | undefined;
-  protected model: string;
-  protected year: number;
-  protected color: string;
-  protected status: boolean;
-  protected buyValue: number;
+export default class Motorcycles extends Vehicle {
   private category: string;
   private engineCapacity: number;
 
   constructor(moto: IMotorcycles) {
+    super(moto);
     this.id = moto.id;
     this.model = moto.model;
     this.year = moto.year;
@@ -21,64 +17,16 @@ export default class Motorcycles {
     this.engineCapacity = moto.engineCapacity;
   }
 
-  public setId(id: string) {
-    this.id = id;
-  }
-
-  public getId() {
-    return this.id;
-  }
-
-  public setModel(car: IMotorcycles) {
-    this.model = car.model;
-  }
-
-  public getModel() {
-    return this.model;
-  }
-
-  public setYear(car: IMotorcycles) {
-    this.year = car.year;
-  }
-
-  public getYear() {
-    return this.year;
-  }
-
-  public setColor(car: IMotorcycles) {
-    this.color = car.color;
-  }
-
-  public getColor() {
-    return this.color;
-  }
-
-  public setStatus(status: boolean) {
-    this.status = status;
-  }
-
-  public getStatus() {
-    return this.status;
-  }
-
-  public setBuyValue(car: IMotorcycles) {
-    this.buyValue = car.buyValue;
-  }
-
-  public getBuyValue() {
-    return this.buyValue;
-  }
-
-  public setCategory(car: IMotorcycles) {
-    this.category = car.category;
+  public setCategory(moto: IMotorcycles) {
+    this.category = moto.category;
   }
 
   public getCategory() {
     return this.category;
   }
 
-  public setEngineCapacity(car: IMotorcycles) {
-    this.engineCapacity = car.engineCapacity;
+  public setEngineCapacity(moto: IMotorcycles) {
+    this.engineCapacity = moto.engineCapacity;
   }
 
   public getEngineCapacity() {
